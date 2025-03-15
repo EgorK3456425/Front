c082927a74e389c79dab584fed0b7da27202af5a
Егор Киняевский <egorkinaevskij@gmail.com>
2025-03-15 14:01:42 +0300

    ToDoMassive

--- ToDoMassive/main.js
+++ ToDoMassive/main.js
@@ -3,16 +3,27 @@ const todo = {
 	"create a new practice task": "In Progress", 
 	"make a bed": "Done", 
 	"write a post": "To Do", },
-addTask(key ,number) { if (key.length > 50)
-    {console.log("NumberMax");
-        return;
-} 
-  
+addTask(key ,number) 
+{
+    if (key.length > 50) {
+        console.log(`${key.length},Слишком большое назание задачи`); 
+};
+    if (this.list[key]) {
+        console.log(`${key}, Задача уже есть`);
+    }
+       
+    
 
         this.list[key] = number;
 },
 changeStatus (key, number) {
-    this.list[key]=number;
+    if (this.list[key]) {
+        console.log(`${key}, Имя задачи есть`);
+    }
+    
+    
+        this.list[key]=number;
+
 },
 deleteTask(key) {
     delete this.list[key];
@@ -24,6 +35,6 @@ showList() {
  
 
 todo.changeStatus("write a post" , "Done");
-todo.addTask("go99999999999999999999999999999999999999999999999999999999999999999999999999999999999999", "CS");
+todo.addTask("Go", "CS");
 todo.deleteTask("make a bed");
 todo.showList();    
